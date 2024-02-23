c57f330115ce3180778aaf2460b2d9e0adb240d3
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2024-02-23 14:17:32 -0500

    Login and Register Page added, AuthService basics added, angular/fire and firebase added to package.json

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    InputTextModule,
+    ButtonModule,
+    CheckboxModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  formGroup!: FormGroup<{
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+    checked: FormControl<boolean | null>;
+  }>;
+  checked: boolean = false;
 
+  emailClassName: string = '';
+  emailSmallClassName: string = 'hide';
+  passwordClassName: string = '';
+  passwordSmallClassName: string = 'hide';
+
+  constructor(private authService: AuthService) {}
+
+  handleForgotPassword() {
+    this.authService.forgotPassword();
+  }
+
+  handleLoginWithEmail() {
+    let emailRegexTest: boolean = this.authService.checkEmailRegex(
+      this.formGroup.value.email!
+    );
+    let passwordRegexTest: boolean = this.authService.checkPasswordRegex(
+      this.formGroup.value.password!
+    );
+
+    if (emailRegexTest) {
+      this.emailSmallClassName = 'hide';
+      this.emailClassName = '';
+    } else {
+      this.emailClassName = 'ng-invalid ng-dirty';
+      this.emailSmallClassName = '';
+    }
+
+    if (passwordRegexTest) {
+      this.passwordClassName = '';
+      this.passwordSmallClassName = 'hide';
+    } else {
+      this.passwordClassName = 'ng-invalid ng-dirty';
+      this.passwordSmallClassName = '';
+    }
+
+    if (emailRegexTest && passwordRegexTest) {
+      this.authService.loginWithEmail(
+        this.formGroup.value.email!,
+        this.formGroup.value.password!
+      );
+    }
+  }
+
+  handleLoginWithGoogle() {
+    this.authService.loginWithGoogle();
+  }
+
+  handleLoginWithGithub() {
+    this.authService.loginWithGithub();
+  }
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      checked: new FormControl(false),
+    });
+  }
 }
--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  emailRegex: RegExp =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  passwordMediumRegex: RegExp =
+    /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+  passwordStrongRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
 
-  constructor() { }
+  constructor() {}
+
+  checkEmailRegex(email: string) {
+    return this.emailRegex.test(email);
+  }
+
+  checkPasswordRegex(password: string) {
+    return (
+      this.passwordMediumRegex.test(password) ||
+      this.passwordStrongRegex.test(password)
+    );
+  }
+
+  signUpWithEmail(email: string, password: string) {
+    console.log(`Signing Up with ${email} and ${password}`);
+  }
+
+  signUpWithGoogle() {
+    console.log('Signing up with google');
+  }
+
+  signUpWithGitHub() {
+    console.log('Signing up in with github');
+  }
+
+  loginWithEmail(email: string, password: string) {
+    console.log(`Logging in with ${email} and ${password}`);
+  }
+
+  loginWithGoogle() {
+    console.log('Logging in with google');
+  }
+
+  loginWithGithub() {
+    console.log('Logging in with github');
+  }
+
+  forgotPassword() {
+    console.log('Handling forgot password users');
+  }
 }
