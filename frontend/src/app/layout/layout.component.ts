61abc27207732946f15ab0db7e3825ed14faebe9
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2024-04-11 23:28:00 -0400

    Removed unused import in layout

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,5 +1,4 @@
 import { Component, effect, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout-ui-components/sidebar/sidebar.component';
 import { PageHeaderComponent } from './layout-ui-components/page-header/page-header.component';
